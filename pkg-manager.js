8dd4659f490586766b3cf2ca96cc5f6c355579a0
zeel <zeel02@gmail.com>
2020-12-20 02:43:14 -0500

    Added code to get icon urls where applicable and store them in the data.

--- pkg-manager.js
+++ pkg-manager.js
@@ -5,6 +5,25 @@ class AdvancedPackageManager extends ModuleManagement {
 			template: "modules/advanced-package-manager/module-management.html",
 		});
 	}
+
+	async getData() {
+		const data = super.getData();
+
+		this.prepareIcons(data);
+
+		return data;
+	}
+
+	prepareIcons(data) {
+		for (let module of data.modules) {
+			if (!module.media) continue;
+			for (let media of module.media) {
+				if (media.type != "icon") continue;
+				module.icon = media.url;
+				break;
+			}
+		}
+	}
 }
 
 Hooks.once("ready", () => {
