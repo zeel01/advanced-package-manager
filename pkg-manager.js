80822343b1e79f5ab2de79561afdcc25b274fd38
zeel <zeel02@gmail.com>
2020-12-20 23:49:16 -0500

    Added JSDoc comments to all methods.

--- pkg-manager.js
+++ pkg-manager.js
@@ -1,5 +1,15 @@
 class AdvancedPackageManager extends ModuleManagement {
+	/** @type {string} */
 	static get namespace() { return "advanced-package-manager"; }
+
+	/**
+	 * Registers game settings for this module.
+	 *
+	 * Registers a setting for application width, and for height.
+	 *
+	 * @static
+	 * @memberof AdvancedPackageManager
+	 */
 	static registerSettings() {
 		game.settings.register(AdvancedPackageManager.namespace, "application-width", {
 			name: "application-width",
@@ -16,14 +26,18 @@ class AdvancedPackageManager extends ModuleManagement {
 			default: "auto"
 		});
 	}
-
-	constructor(...args) {
-		super(...args);
-
-		//
-	}
 	
-	/** @override */
+	
+	/**
+	 * This object contains the initial options for the application.
+	 *
+	 * They may be overridden by options passed to the constructor.
+	 *
+	 * @override
+	 * @readonly
+	 * @static
+	 * @memberof AdvancedPackageManager
+	 */
 	static get defaultOptions() {
 		return mergeObject(super.defaultOptions, {
 			template: "modules/advanced-package-manager/module-management.html",
@@ -33,6 +47,13 @@ class AdvancedPackageManager extends ModuleManagement {
 		});
 	}
 
+	/**
+	 * Produces an object of data that will be passed
+	 * to the application template.
+	 *
+	 * @return {object} The data for rendering the template.
+	 * @memberof AdvancedPackageManager
+	 */
 	async getData() {
 		const data = super.getData();
 
@@ -42,6 +63,16 @@ class AdvancedPackageManager extends ModuleManagement {
 
 		return data;
 	}
+
+	/**
+	 * This method is called when the resize handle is
+	 * used to adjust the size of the application.
+	 *
+	 * Saves the width and height to game settings.
+	 *
+	 * @override
+	 * @memberof AdvancedPackageManager
+	 */
 	async _onResize() {
 		await game.settings.set(
 			this.constructor.namespace,
@@ -55,6 +86,13 @@ class AdvancedPackageManager extends ModuleManagement {
 		);
 	}
 
+	/**
+	 * Loads two SVG images as if they were HTML/hbs tempate files.
+	 * 
+	 * Makes it possible to easily embed them in the HTML later.
+	 *
+	 * @memberof AdvancedPackageManager
+	 */
 	async prepareSVGs() {
 		await loadTemplates([
 			"modules/advanced-package-manager/assets/cardboard-box.hbs",
@@ -62,6 +100,15 @@ class AdvancedPackageManager extends ModuleManagement {
 		]);
 	}
 	
+	/**
+	 * Prepares the `authors` object on each module.
+	 *
+	 * Some modules only have an `author` field, creates
+	 * a new `authors` object with the name of the author.
+	 *
+	 * @param {data} data - The template data from getData
+	 * @memberof AdvancedPackageManager
+	 */
 	prepareAuthors(data) {
 		for (let module of data.modules) {
 			if (module.authors?.length) continue;
@@ -70,6 +117,18 @@ class AdvancedPackageManager extends ModuleManagement {
 			];
 		}
 	}
+
+	/**
+	 * Prepares the information needed to display a
+	 * modules icon.
+	 *
+	 * Locates the media property, and looks for an "icon".
+	 * If an icon is found, stores the url or "link" value
+	 * in the data object.
+	 *
+	 * @param {object} data - The template data from getData
+	 * @memberof AdvancedPackageManager
+	 */
 	prepareIcons(data) {
 		for (let module of data.modules) {
 			if (!module.media) continue;
@@ -85,7 +144,11 @@ class AdvancedPackageManager extends ModuleManagement {
 
 Hooks.once("init", AdvancedPackageManager.registerSettings);
 
-/******************************************************************/
+/****************************************************************** 
+
+The following are for debugging purposes only.
+
+******************************************************************/
 
 Hooks.once("ready", () => {
 	new AdvancedPackageManager().render(true);
@@ -96,3 +159,6 @@ Hooks.on("renderAdvancedPackageManager", (manager, html, data) => {
 	console.debug(html);
 	console.debug(data);
 });
+
+/****************************************************************** 
+******************************************************************/
